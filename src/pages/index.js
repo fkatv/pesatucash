37f9db86a47fd9616783c0257e903832309def53
Fabián Catalán <f.catalan.v@gmail.com>
2021-07-29 16:42:08 -0400

    Version simple 1 (#1)
    * taraje updated, monedas de un tipo, (falta ajustar $500) v1.0.3
    
    * update 'coins calculator'
    
    * Short Documentation
    
    * adjust docs v(1.0.5)

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,70 +14,6 @@ const headingStyles = {
 const headingAccentStyles = {
   color: "#663399",
 }
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
 
 // markup
 const IndexPage = () => {
@@ -85,56 +21,74 @@ const IndexPage = () => {
   const [tara, setTara] = useState(0);
   const [peso, setPeso] = useState(0);
   const [total, setTotal] = useState(0);
-  const [esToggled, setToggle] = useState([false, false, false, false])
+  const [monedaFocus, setMonedaFocus] = useState(0)
+  const [resultado_count, setRCount] = useState("")
   const monedas_label = ['$10','$50','$100','$500']
-  const monedas_valor = [10,50,100,500]
 
   useEffect(()=>{})
 
   const calcular = () => {
-    const valor = calculator(tara, peso, [0]);
-    setTotal(valor)
+    const data = calculator(tara, peso, monedaFocus);
+    console.log(data)
+    if (data) {
+      const txt = data[0]
+      const valor = data[1]
+      setTotal(valor)
+      setRCount(txt)
+    }
   }
 
-  const changeToggle = (i) => {
-    let temp = esToggled
-    temp[i] = !temp[i]
-    setToggle(temp)
-  }
+
 
   return (
     <main style={pageStyles}>
-      <title>Test fff</title>
+      <title>Pesa Cash</title>
       <h1 style={headingStyles}>
-        Moneas
+        Pesa Cash
+        <span>
+        <img
+          alt="Gatsby G Logo"
+          src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+        />
+        </span>
         <br />
         <span style={headingAccentStyles}>— Laurelito Project! </span>
       </h1>
 
       <h2>
-        Escoja la(s) moneda(s)
+        Escoja la moneda a pesar
         <br/>
         {monedas_label.map((item, i) => (
-          <button key={i} onClick={() => changeToggle(i)} style={{color:esToggled[i] ? 'green' : "black"}}>{item}</button>
+          <button key={i} onClick={() => setMonedaFocus(i)} style={{color:monedaFocus === i ? 'green' : "black"}}>{item}</button>
         ))}
       </h2>
 
       <label>
         Tara en gramos
-        <input type="number" placeholder="Tara" value = {tara} onChange = {(e) => setTara(e.target.value)}/>
+        <br/>
+        <input type="number" placeholder="Tara" min={0} value = {tara} onChange = {(e) => setTara(e.target.value)}/>
       </label>
+
+      <br />
 
       <label>
         Peso Total en gramos
-        <input type="number" placeholder="Peso total" value = {peso} onChange = {(e) => setPeso(e.target.value)}/>
+        <br/>
+        <input type="number" placeholder="Peso total" min={0} value = {peso} onChange = {(e) => setPeso(e.target.value)}/>
       </label>
+
       <h2>${total}</h2>
+      <h3>{resultado_count}</h3>
+
       <button onClick={()=> calcular()}>calcular</button>
 
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <br/>
+
+      <footer>
+      <br/><br/>
+      <a href="https://github.com/fkatv/Laurelito"> Ésta página es de Código abierto sin fines de lucro ni analíticas </a>
+      </footer>
+
     </main>
   )
 }
--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -2,8 +2,9 @@ const monedas = [
   {"nombre":"$10","peso":3.5, "valor":10, "imagen":""},
   {"nombre":"$50","peso":7.0, "valor":50, "imagen":""},
   {"nombre":"$100 mapuche","peso":7.58, "valor":100, "imagen":""},
-  {"nombre":"$100 antigua","peso":9, "valor":100, "imagen":""},
   {"nombre":"$500","peso":6.5, "valor":500, "imagen":""},
+  {"nombre":"$100 antigua","peso":9, "valor":100, "imagen":""},
+
 ]
 /** *
 Retorna numero de monedas y su valor en un pesaje de monedas comprobatorio.
@@ -17,7 +18,6 @@ const estimaMoneys = (peso, gramo_monedas) => {
 }
 
 const calculaValor = (x,y,m1,m2) => {
-  alert(x+""+y)
   return x*m1+y*m2
 }
 
@@ -25,61 +25,94 @@ const eAbsoluto = (v) => {
   return v - parseInt(v)
 }
 
+/** *
+  Verifica si el residuo entre las dos cantidades de monedas a
+  testear es menor al doble de la tolerancia para poder TARAR mejor.
+  vs 1.0.5
+*/
 const prueba_y_Error = (i,j,m1,m2,P) => {
-  test = Math.abs(i*m1+j*m2 -P)
-  if (-tolerancia <= test < tolerancia) return true
-  return false
+  const test = Math.abs(i*m1+j*m2 - P)
+  console.log(i,j, test)
+  if ( test < tolerancia*2) return true
+  else return false
 }
 
-const extraeValor_Peso = (M) => {
-  let coins = M.map((item, i) => monedas[item])
-  return [ coins.map((i) => i.valor), coins.map((i) => i.peso) ]
-}
-
+/** *
+  Según la tara, el peso de las monedas, y la Moneda a calcular,
+  determina la cantidad y el valor de dichas monedas.
+*/
 const calcularMonto = (tara, P, M) => {
-  const data = extraeValor_Peso(M);
-  const m_valor = data[0]
-  const m_peso = data[1]
+
+  const m_nombre = monedas[M].nombre
+  const m_valor = monedas[M].valor
+  const m_peso = monedas[M].peso
   P = Math.abs(P - tara)
-  const _x = estimaMoneys(P, m_peso[0])
+  const _x = estimaMoneys(P, m_peso)
   const e_x = eAbsoluto(_x)
-  if (m_valor !== 100){
-    if (e_x ==0 || -tolerancia <= e_x <= tolerancia) {
-      return valorCondicionado(e_x,_x, m_valor[0])
+
+
+  if ( m_valor !== 100){
+    if (e_x === 0 || -tolerancia <= e_x <= tolerancia) {
+      return valorCondicionado(e_x,_x, m_valor, m_nombre)
     } else {
-        // for (const i of Array(Math.ceil(_x)+1).keys()) {
-        //   let test = Math.abs(i*m_peso[0] -P)
-        //   if (-tolerancia <= test < tolerancia) return i*m_valor
-        // }
-        return parseInt(_x)*m_valor[0]
-      }
+        const txt = parseInt(_x) + " moneda(s)* de " + m_nombre
+        return [txt , parseInt(_x) * m_valor ]
     }
-    else
+  }
+  else
     {
       // para las dos monedas de 100
-      const _y = estimaMoneys(P, m_peso[0])
-      const e_y = eAbsoluto(_y)
+      const m100_antigua = monedas[4]
+      const m100_mapu = monedas[2]
+      return calcularSiHayDosTiposMoneda(P,_x,e_x,100,m100_mapu, m100_antigua);
+    }
+    alert("Sus monedas tienen scotchs o estan con peso extra / o su balanza está demasiado descalibrada")
+    return(['#Mala medición, recalibre', 0])
+  }
 
-      if (e_x === 0 || e_y === 0) {
-        if(-tolerancia <= e_x <= tolerancia){
-          return valorCondicionado(e_x, _x, m_valor[0])
-        } else {
-          return valorCondicionado(e_y, _y, m_valor[0])
-        }
+  /** *
+    Calcula la cantidad y el valor final de las monedas combinadas de un
+    valor único
+    vs 1.0.5
+  */
+  const calcularSiHayDosTiposMoneda = (P,_x,e_x,m_valor,moneda1,moneda2) => {
+    const mpeso1 = moneda1.peso
+    const mpeso2 = moneda2.peso
+    const _y = estimaMoneys(P, mpeso2)
+    const e_y = eAbsoluto(_y)
+
+    if (e_x === 0 || e_y === 0) {
+      if(-tolerancia <= e_x <= tolerancia){
+        return valorCondicionado(e_x, _x, m_valor, moneda1.nombre)
       } else {
-        for (const i of Array(Math.ceil(_x)+1).keys()) {
-          for (const j of Array(Math.ceil(_y)+1).keys()) {
-            if (prueba_y_Error(i,j,9,7.58,P)) return calculaValor(i,j,100,100)
+        return valorCondicionado(e_y, _y, m_valor, moneda2.nombre)
+      }
+    } else {
+      console.log('entro a calcular los 100')
+      for (const i of Array(Math.ceil(_x)+1).keys()) {
+        for (const j of Array(Math.ceil(_y)+1).keys()) {
+          if (prueba_y_Error(i,j,mpeso1,mpeso2,P)){
+            return [`${i} moneda(s) nuevas y ${j} moneda(s) antiguas.`,calculaValor(i,j,100,100)]
+          }
+          if (prueba_y_Error(j,i,mpeso2,mpeso1,P)){
+            return [`${j} moneda(s) nuevas y ${i} moneda(s) antiguas.`,calculaValor(i,j,100,100)]
           }
         }
       }
+      alert("Sus monedas tienen scotchs o estan con peso extra / o su balanza está demasiado descalibrada")
+      return(['*Mala medición, recalibre',0])
     }
   }
 
-  const valorCondicionado = (e_, __x, valor) => {
+  /** *
+    Por ajustes de ensayo y error, esta función  transforma la cantidad de
+    monedas ya calculada, ajustandolo a un valor cercano a lo esperado.
+    vs 1.0.5
+  */
+  const valorCondicionado = (e_, __x, valor, coin_name) => {
     if(e_ < 0) __x += tolerancia
     const x = parseInt(__x)
-    return x * valor
+    return [Math.ceil(__x) + " moneda(s) de "+coin_name, x * valor]
   }
 
 
